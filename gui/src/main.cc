ddebe21c45ce526c4cc75c95e5a894d6139511c7
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-06-16 16:43:18 +0200

    [WIP] Link against MoMEMta

--- gui/src/main.cc
+++ gui/src/main.cc
@@ -2,7 +2,23 @@
 
 #include <MainWindow.h>
 
+#include <momemta/ModuleDescription.h>
+#include <momemta/ModuleFactory.h>
+
+#include <iostream>
+
 int main(int argc, char **argv) {
+
+    auto plugins = ModuleDescriptionFillerFactory::get().getPluginsList();
+    std::cout << "Available plugins: " << std::endl;
+    for (const auto& plugin: plugins)
+        std::cout << "    - " << plugin << std::endl;
+
+    auto test = ModuleDescriptionFillerFactory::get().create("BlockD");
+
+    ModuleDescription description;
+    test->fillDescription(description);
+
     QApplication app(argc, argv);
 
     MainWindow *window = new MainWindow;
